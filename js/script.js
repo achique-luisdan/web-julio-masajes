807744608b57e1b0fc6b50cf20d2b9e1730e73ba
achique-luisdan <achique.luisdan@gmail.com>
2022-10-30 19:32:14 -0400

    Completa sección hero animada

--- js/script.js
+++ js/script.js
@@ -78,3 +78,88 @@ function selectInformation (index){
 renderCategories()
 
 showInformation();
+
+
+function generateNumber (max, min=1){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+class ElementUI {
+  column = null;
+  row = null;
+}
+
+class Rectangle extends ElementUI {
+  color =  null;
+  generatePosition(){
+    let num1 =  generateNumber(10);
+    let num2 = generateNumber(8);
+    super.column = null;
+    super.row = null;
+    while(notRepeat(num1, num2)){
+      if (notRepeat(num1, num2)) {
+      num1 =  generateNumber(10);
+      num2 = generateNumber(8);
+      } else {
+        this.column = num1;
+        this.row = num2;
+        break;
+      }
+    }
+    super.column = num1;
+    super.row = num2;
+
+    this.generateColor();
+    if (this.column >= 3 && this.column <= 9){
+      if (this.row >=3 && this.row <= 6){
+        this.color = 'transparent'
+      }
+    }
+  }
+  generateColor(){
+    const num = generateNumber(4);
+    if (num === 1){
+      this.color = "primary";
+    } else if (num === 2){
+      this.color = "secondary";
+    } else if (num === 3){
+      this.color = "on-surface";
+    }
+    else if (num === 4){
+      this.color = "transparent";
+    }
+  }
+}
+
+
+var rectangles = new Array();
+for (let i= 0; i < 80; i = i +1){
+  let rectangle = new Rectangle();
+  rectangles.push(rectangle);
+}
+
+function notRepeat(column, row){
+  return rectangles.filter (rectangle => {
+    return rectangle.column === column && rectangle.row === row;
+  }).length > 0;
+}
+
+function existent(column, row){
+  return rectangles.filter (rectangle => {
+    return rectangle.column === column && rectangle.row === row;
+  }).length
+}
+
+
+function animation(){
+  let rectanglesUI = document.getElementsByClassName('rectangle');
+  for (let i= 0; i < 80; i = i +1){
+    rectangles[i].generatePosition();
+    rectanglesUI[i].style.gridColumn = rectangles[i].column;
+    rectanglesUI[i].style.gridRow = rectangles[i].row;
+    rectanglesUI[i].className = 'rectangle'
+    rectanglesUI[i].className += ` show ${rectangles[i].color}`;
+  }
+}
+
+setInterval(animation, 2000);
